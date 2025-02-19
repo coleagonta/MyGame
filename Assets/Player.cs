4920c27af51761d3398c346ffd1e1f043e622540
PC <colea.w8@gmail.com>
2025-02-19 19:03:48 +0200

    Fix animation Player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float rotateSpeed = 7f; // добавим скорость поворота
+    [SerializeField] private float rotateSpeed = 7f; // скорость поворота
     [SerializeField] private float jumpHeight = 5f; 
     [SerializeField] private GameInput _gameInput;
     [SerializeField] private Animator animator;
@@ -28,10 +28,8 @@ public class Player : MonoBehaviour
         Vector2 inputVector = _gameInput.GetMovmentVectorNormalized();
         Vector3 moveDir = new Vector3(-inputVector.y, 0f, inputVector.x);
 
-        // Двигаем персонажа
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         
-        // Проверяем, идет ли персонаж
         isWalking = moveDir.magnitude > 0.1f; 
 
         if (animator != null)
@@ -53,9 +51,13 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        animator.SetBool("jump", true);
         rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
         isGrounded = false;
+        
+        if (animator != null)
+        {
+            animator.SetBool("jump", true);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -63,7 +65,11 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true; 
-            animator.SetBool("jump", false);
+            
+            if (animator != null)
+            {
+                animator.SetBool("jump", false); // Выключаем анимацию прыжка при приземлении
+            }
         }
     }
 
